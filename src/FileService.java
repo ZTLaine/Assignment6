aeac102c0a0d63073fc0ff2d3166853064ae8aa1
Zack Laine <rumtehk_uh@yahoo.com>
2024-05-04 10:40:33 -0400

    Working on readFile

--- src/FileService.java
+++ src/FileService.java
@@ -1,14 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.YearMonth;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FileService {
     List<CarModel> readFiles(List<String> fileNames){
         List<CarModel> salesData = new ArrayList<>();
 
         for(String file : fileNames){
-//            Read the file here
-            CarModel car = new CarModel(file);
-
+            try(BufferedReader bufferedReader = new BufferedReader(new FileReader(file))){
+                bufferedReader.readLine();
+                CarModel car = new CarModel(file);
+                Map<YearMonth, Integer> sales = new LinkedHashMap<>();
+                while (bufferedReader.ready()) {
+                    String[] salesInput = bufferedReader.readLine().split(",");
+//                    YearMonth saleMonth = ;
+//                    sales.put(saleMonth,salesInput[1]);
+                }
+            }
+            catch (IOException e){
+                System.out.println("File error with " + file + ": " + e.getMessage());
+            }
         }
         return salesData;
     }
