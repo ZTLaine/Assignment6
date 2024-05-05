5acee9e1bb018d5406e86c2cdb12437edb0f741e
Zack Laine <rumtehk_uh@yahoo.com>
2024-05-05 11:17:42 -0400

    Refactoring a lil bit

--- src/FileService.java
+++ src/FileService.java
@@ -2,30 +2,33 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.YearMonth;
-import java.util.ArrayList;
+import java.time.format.DateTimeFormatter;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FileService {
-    List<CarModel> readFiles(List<String> fileNames){
-        List<CarModel> salesData = new ArrayList<>();
+    CarModel readFiles(List<String> fileNames){
+        CarModel car;
 
         for(String file : fileNames){
             try(BufferedReader bufferedReader = new BufferedReader(new FileReader(file))){
                 bufferedReader.readLine();
-                CarModel car = new CarModel(file);
+                car = new CarModel(file);
                 Map<YearMonth, Integer> sales = new LinkedHashMap<>();
                 while (bufferedReader.ready()) {
                     String[] salesInput = bufferedReader.readLine().split(",");
-//                    YearMonth saleMonth = ;
-//                    sales.put(saleMonth,salesInput[1]);
+                    YearMonth saleMonth = YearMonth.parse(salesInput[0], DateTimeFormatter.ofPattern("MMM-yy"));
+                    sales.put(saleMonth,Integer.valueOf(salesInput[1]));
                 }
+                car.setMonthlySales(sales);
+                return car;
             }
             catch (IOException e){
                 System.out.println("File error with " + file + ": " + e.getMessage());
             }
         }
-        return salesData;
+//        This feels sloppy
+        return null;
     }
 }
---
+++ src/SalesReportService.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SalesReportService {
+    List<CarModel> carModelData = new ArrayList<>();
+
+    void report(){
+        FileService fileService = new FileService();
+    }
+}
